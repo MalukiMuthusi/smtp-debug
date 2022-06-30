6948b7f93063f85354ddf6580deaea4f247dd562
Maluki Muthusi <malukimuthusi@gmail.com>
2022-06-30 10:02:53 +0300

    Shorten the code

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/MalukiMuthusi/smtp-debug
 
 go 1.18
 
-require golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e
+require (
+	github.com/emersion/go-smtp v0.15.0
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e
+)
 
-require golang.org/x/text v0.3.7 // indirect
+require (
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"errors"
 	"log"
 	"net"
-	"net/smtp"
 	"net/url"
 	"time"
 
+	smtp "github.com/emersion/go-smtp"
 	"golang.org/x/net/idna"
 	"golang.org/x/net/proxy"
 )
@@ -26,7 +26,7 @@ func main() {
 	// The code works when I use a localhost proxy
 	//   	socks5://dante:maluki@127.0.0.1:1080
 
-	client, err := newSMTPClient("gmail.com", "socks5://dante:maluki@35.242.186.23:1080")
+	client, err := newSMTPClient("gmail.com", "socks5://dante:maluki@127.0.0.1:1080")
 
 	if err != nil {
 		log.Println(err)
@@ -36,49 +36,16 @@ func main() {
 	log.Println(client)
 }
 
-// establishProxyConnection connects to the address on the named network address
-// via proxy protocol
-func establishProxyConnection(addr, proxyURI string) (net.Conn, error) {
-	// return socks.Dial(proxyURI)("tcp", addr)
+// newSMTPClient generates a new available SMTP client
+func newSMTPClient(domain, proxyURI string) (*smtp.Client, error) {
 
-	u, err := url.Parse(proxyURI)
+	domain, err := idna.ToASCII(domain)
 	if err != nil {
 		log.Println(err)
 
 		return nil, err
 	}
 
-	var iface proxy.Dialer
-
-	if u.User != nil {
-		auth := proxy.Auth{}
-
-		auth.User = u.User.Username()
-		auth.Password, _ = u.User.Password()
-
-		iface, err = proxy.SOCKS5("tcp", u.Host, &auth, &net.Dialer{Timeout: 30 * time.Second})
-		if err != nil {
-			log.Println(err)
-
-			return nil, err
-		}
-	} else {
-		iface, err = proxy.SOCKS5("tcp", u.Host, nil, proxy.FromEnvironment())
-		if err != nil {
-			log.Println(err)
-
-			return nil, err
-		}
-	}
-
-	dialfunc := iface.Dial
-
-	return dialfunc("tcp", addr)
-}
-
-// newSMTPClient generates a new available SMTP client
-func newSMTPClient(domain, proxyURI string) (*smtp.Client, error) {
-	domain = domainToASCII(domain)
 	mxRecords, err := net.LookupMX(domain)
 	if err != nil {
 		log.Println(err)
@@ -113,64 +80,62 @@ func newSMTPClient(domain, proxyURI string) (*smtp.Client, error) {
 // SMTP server (socks5 proxy supported) and fails with a timeout if timeout is reached while
 // attempting to establish a new connection
 func dialSMTP(addr, proxyURI string) (*smtp.Client, error) {
-	// Channel holding the new smtp.Client or error
-	ch := make(chan interface{}, 1)
 
 	// Dial the new smtp connection
-	go func() {
-		var conn net.Conn
-		var err error
 
-		conn, err = establishProxyConnection(addr, proxyURI)
-		if err != nil {
-			log.Println(err)
-		}
-
-		if err != nil {
-			ch <- err
-			return
-		}
-
-		host, _, err := net.SplitHostPort(addr)
-		if err != nil {
-			log.Println(err)
-		}
-
-		client, err := smtp.NewClient(conn, host)
-		log.Println(client)
-
-		if err != nil {
-			log.Println(err)
-
-			ch <- err
-			return
-		}
-		ch <- client
-	}()
-
-	// Retrieve the smtp client from our client channel or timeout
-	select {
-	case res := <-ch:
-		switch r := res.(type) {
-		case *smtp.Client:
-			return r, nil
-		case error:
-			return nil, r
-		default:
-			return nil, errors.New("Unexpected response dialing SMTP server")
-		}
-	case <-time.After(smtpTimeout):
-		return nil, errors.New("Timeout connecting to mail-exchanger")
-	}
-}
-
-// domainToASCII converts any internationalized domain names to ASCII
-// reference: https://en.wikipedia.org/wiki/Punycode
-func domainToASCII(domain string) string {
-	asciiDomain, err := idna.ToASCII(domain)
+	u, err := url.Parse(proxyURI)
 	if err != nil {
-		return domain
-	}
-	return asciiDomain
+		log.Println(err)
 
+		return nil, err
+	}
+
+	var iface proxy.Dialer
+
+	if u.User != nil {
+		auth := proxy.Auth{}
+
+		auth.User = u.User.Username()
+		auth.Password, _ = u.User.Password()
+
+		iface, err = proxy.SOCKS5("tcp", u.Host, &auth, &net.Dialer{Timeout: 30 * time.Second})
+		if err != nil {
+			log.Println(err)
+
+			return nil, err
+		}
+	} else {
+		iface, err = proxy.SOCKS5("tcp", u.Host, nil, proxy.FromEnvironment())
+		if err != nil {
+			log.Println(err)
+
+			return nil, err
+		}
+	}
+
+	dialfunc := iface.Dial
+
+	netConn, err := dialfunc("tcp", addr)
+	if err != nil {
+		log.Println(err)
+
+		return nil, err
+	}
+
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Println(err)
+
+		return nil, err
+	}
+
+	client, err := smtp.NewClient(netConn, host)
+
+	if err != nil {
+		log.Println(err)
+
+		return nil, err
+	}
+
+	return client, nil
 }
